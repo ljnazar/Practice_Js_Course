4b29a415a64bea8d4cb7d71878ed20a167b153a3
Leandro Jesús Nazar <ljnazar@gmail.com>
2022-08-21 16:52:40 -0300

    Add files via upload

---
+++ Leandro-Nazar-Desafio-1/app.js
@@ -0,0 +1,34 @@
+const candidatoA = "Will Smith";
+const candidatoB = "Stephen king";
+const candidatoC = "Robert Lawrence";
+
+let conteoA = 0;
+let conteoB = 0;
+let conteoC = 0;
+
+let ingresoVoto = prompt("¿Desea ingresar un voto? (si/no)");
+
+while(ingresoVoto.toLowerCase() !== "no"){
+    let nombre = prompt("Ingrese su nombre:");
+    let voto = prompt(`Ingrese su voto:\nA - ${candidatoA}\nB - ${candidatoB}\nC - ${candidatoC}`);
+    switch(voto.toUpperCase()){
+        case "A":
+            alert("Usted voto por Will Smith");
+            conteoA++;
+            break;
+        case "B":
+            alert("Usted voto por Stephen king");
+            conteoB++;
+            break;
+        case "C":
+            alert("Usted voto por Robert Lawrence");
+            conteoC++;
+            break;
+        default:
+            alert("Opción no válida");
+            break;
+    }
+    ingresoVoto = prompt("¿Desea ingresar otro voto? (si/no)");
+}
+
+alert(`Resultados:\n${candidatoA}: ${conteoA} votos\n${candidatoB}: ${conteoB} votos\n${candidatoC}: ${conteoC} votos`)
