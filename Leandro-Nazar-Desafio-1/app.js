21142478546f5a354261686c3f4ac97468ec5af8
Leandro Nazar <ljnazar@gmail.com>
2022-08-21 23:05:07 -0300

    add_if-else_validation

--- Leandro-Nazar-Desafio-1/app.js
+++ Leandro-Nazar-Desafio-1/app.js
@@ -6,29 +6,35 @@ let conteoA = 0;
 let conteoB = 0;
 let conteoC = 0;
 
-let ingresoVoto = prompt("¿Desea ingresar un voto?(si/no)");
+let ingresoVoto = prompt("¿Desea ingresar un voto? (si/no)");
 
 while(ingresoVoto.toLowerCase() !== "no"){
     let nombre = prompt("Ingrese su nombre:");
-    let voto = prompt(`Ingrese su voto:\nA - ${candidatoA}\nB - ${candidatoB}\nC - ${candidatoC}`);
-    switch(voto.toUpperCase()){
-        case "A":
-            alert("Usted voto por Will Smith");
-            conteoA++;
-            break;
-        case "B":
-            alert("Usted voto por Stephen king");
-            conteoB++;
-            break;
-        case "C":
-            alert("Usted voto por Robert Lawrence");
-            conteoC++;
-            break;
-        default:
-            alert("Opción no válida");
-            break;
+    if(nombre !== ""){
+        alert(`Bienvenido ${nombre}`);
+        let voto = prompt(`Ingrese su voto:\nA - ${candidatoA}\nB - ${candidatoB}\nC - ${candidatoC}`);
+        switch(voto.toUpperCase()){
+            case "A":
+                alert("Usted voto por Will Smith");
+                conteoA++;
+                break;
+            case "B":
+                alert("Usted voto por Stephen king");
+                conteoB++;
+                break;
+            case "C":
+                alert("Usted voto por Robert Lawrence");
+                conteoC++;
+                break;
+            default:
+                alert("Opción no válida");
+                break;
+        }
+        ingresoVoto = prompt("¿Desea ingresar otro voto? (si/no)");
     }
-    ingresoVoto = prompt("¿Desea ingresar otro voto? (si/no)");
+    else{
+        alert("Nombre no ingresado")
+    }
 }
 
 alert(`Resultados:\n${candidatoA}: ${conteoA} votos\n${candidatoB}: ${conteoB} votos\n${candidatoC}: ${conteoC} votos`)
